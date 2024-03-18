6f15e9da0f6e8deb10372136d47dfbeff17af517
Johan Nordström <johan@digitalvillage.se>
2024-03-18 13:16:05 +0100

    Test random number generator.

--- 01-unit-testing/src/randomNumber.test.ts
+++ 01-unit-testing/src/randomNumber.test.ts
@@ -1,6 +1,27 @@
 import { it, expect } from "vitest";
 import { getRandomNumber } from "./randomNumber";
 
-it.todo("generates a random number between 1-10");
+it("generates a random number between 1-10", () => {
+	const randomNumber = getRandomNumber();
 
-it.todo("generates a random number between 1-50");
+	expect(randomNumber).toBeGreaterThanOrEqual(1);
+	expect(randomNumber).toBeLessThanOrEqual(10);
+});
+
+it("generates a random number between 1-50", () => {
+	const max = 50;
+	const randomNumber = getRandomNumber(max);
+
+	expect(randomNumber).toBeGreaterThanOrEqual(1);
+	expect(randomNumber).toBeLessThanOrEqual(max);
+});
+
+it("generates random number between 1-10", () => {
+	for (let i = 0; i < 10; i++) {
+		const max = 10;
+		const randomNumber = getRandomNumber(max);
+
+		expect(randomNumber).toBeGreaterThanOrEqual(1);
+		expect(randomNumber).toBeLessThanOrEqual(max);
+	}
+});
--- 01-unit-testing/src/randomNumber.ts
+++ 01-unit-testing/src/randomNumber.ts
@@ -1,3 +1,8 @@
-export const getRandomNumber = () => {
-	return Math.ceil(Math.random() * 10);
+/**
+ * Returns a random number between 1..max
+ *
+ * @param max Largest number to return
+ */
+export const getRandomNumber = (max = 10) => {
+	return Math.ceil(Math.random() * max);
 }
