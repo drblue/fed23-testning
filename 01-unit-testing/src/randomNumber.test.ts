88b82207c80416e94a0c1913b9d1411b4c8ce56f
Johan Nordström <johan@digitalvillage.se>
2024-03-18 11:17:15 +0100

    Test random number generator (nano-workshop).

---
+++ 01-unit-testing/src/randomNumber.test.ts
@@ -0,0 +1,6 @@
+import { it, expect } from "vitest";
+import { getRandomNumber } from "./randomNumber";
+
+it.todo("generates a random number between 1-10");
+
+it.todo("generates a random number between 1-50");
---
+++ 01-unit-testing/src/randomNumber.ts
@@ -0,0 +1,3 @@
+export const getRandomNumber = () => {
+	return Math.ceil(Math.random() * 10);
+}
