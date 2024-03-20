909a7914f3777947b3a4784109e5f516bf1bad15
Johan Nordström <johan@digitalvillage.se>
2024-03-20 09:52:39 +0100

    Test-skeleton for todoStorage.

---
+++ 02-mocking/src/tests/todoStorage.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { getTodos, saveTodos } from "../utils/todoStorage";
+
+describe("get todos", () => {
+	it.todo("returns empty list of todos", () => {});
+});
+
+describe("save todos", () => {
+	it.todo("can save a todo", () => {});
+
+	it.todo("can save a todo and then retrieve it", () => {});
+});
