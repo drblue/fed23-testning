a95ce2f193cddb77cd0b9d30aa197e6f403768ae
Johan Nordström <johan@digitalvillage.se>
2024-03-20 09:52:33 +0100

    Fixed formatting and indentation in utils.

--- 02-mocking/src/utils/render.ts
+++ 02-mocking/src/utils/render.ts
@@ -5,9 +5,8 @@ export const renderTodos = (todos: Todo[]) => {
 	// replace todosList content
 	const todosEl = document.querySelector<HTMLUListElement>("#todos")!;
 	todosEl.innerHTML = todos
-		.map(
-			(todo) =>
-				`<li class="list-group-item todo ${todo.completed ? 'completed' : ''}" data-todo-id="${todo.id}">
+		.map((todo) =>
+			`<li class="list-group-item todo ${todo.completed ? 'completed' : ''}" data-todo-id="${todo.id}">
 				<span class="todo-title">${todo.title}</span>
 				${todo.completed
 					? '<span class="delete-todo" role="button" title="Delete todo">🗑️</span>'
--- 02-mocking/src/utils/todoStorage.ts
+++ 02-mocking/src/utils/todoStorage.ts
@@ -1,5 +1,5 @@
-import DOMException from 'domexception'
-import { Todo } from '../types/Todo'
+import DOMException from "domexception";
+import { Todo } from "../types/Todo";
 
 /**
  * Get todos from localStorage
@@ -7,13 +7,13 @@ import { Todo } from '../types/Todo'
  */
 export const getTodos = (): Todo[] => {
 	// get json-todos from localStorage
-	const json = localStorage.getItem('todos') ?? '[]'
+	const json = localStorage.getItem("todos") ?? "[]";
 
 	// parse json-todos into an array of Todo-objects
-	const todos: Todo[] = JSON.parse(json)
+	const todos: Todo[] = JSON.parse(json);
 
-	return todos
-}
+	return todos;
+};
 
 /**
  * Save todos to localStorage
@@ -23,14 +23,14 @@ export const getTodos = (): Todo[] => {
 export const saveTodos = (todos: Todo[]) => {
 	try {
 		// convert todos-array to JSON
-		const json = JSON.stringify(todos)
+		const json = JSON.stringify(todos);
 
 		// save JSON to localStorage
-		localStorage.setItem('todos', json)
+		localStorage.setItem("todos", json);
 
 		return {
 			success: true,
-		}
+		};
 
 	} catch (error) {
 		// if DomainException is thrown, return error
@@ -38,13 +38,13 @@ export const saveTodos = (todos: Todo[]) => {
 			return {
 				success: false,
 				error: error.message,
-			}
+			};
 		}
 
 		// return unknown error
 		return {
 			success: false,
-			error: 'Unknown error',
-		}
+			error: "Unknown error",
+		};
 	}
-}
+};
