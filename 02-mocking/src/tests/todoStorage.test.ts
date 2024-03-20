87d20bae3b0657d2e74720beb6b371112dfcc1a2
Johan Nordström <johan@digitalvillage.se>
2024-03-20 11:28:45 +0100

    Working mock implementation of localStorage + mini-workshop.

--- 02-mocking/src/tests/todoStorage.test.ts
+++ 02-mocking/src/tests/todoStorage.test.ts
@@ -1,12 +1,44 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import mockedLocalStorage from "../mocks/mockedLocalStorage";
 import { getTodos, saveTodos } from "../utils/todoStorage";
+import { Todo } from "../types/Todo";
+
+// Reference to the original localStorage
+let originalLocalStorage: any;
+
+const TODO: Todo = {
+	id: 1,
+	title: "My first todo",
+	completed: false,
+}
+
+beforeEach(() => {
+	// Save a reference to the original localStorage before each test
+	originalLocalStorage = globalThis.localStorage;
+
+	// Replace localStorage with the mocked version
+	globalThis.localStorage = mockedLocalStorage();
+});
+
+afterEach(() => {
+	// Restore localStorage to the original version
+	globalThis.localStorage = originalLocalStorage;
+});
 
 describe("get todos", () => {
-	it.todo("returns empty list of todos", () => {});
+	it("returns empty list of todos", () => {
+		const todos = getTodos();
+
+		expect(todos.length).toBe(0);
+	});
 });
 
 describe("save todos", () => {
-	it.todo("can save a todo", () => {});
+	it.todo("can save a todo", () => {
+		// save TODO and make sure its ok
+	});
 
-	it.todo("can save a todo and then retrieve it", () => {});
+	it.todo("can save a todo and then retrieve it", () => {
+		// save TODO and then make sure we can retrieve it
+	});
 });
