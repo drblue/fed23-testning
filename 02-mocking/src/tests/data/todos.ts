2baa36dab251bc0f6096ef70b62219d3293c752c
Johan Nordström <johan@digitalvillage.se>
2024-03-21 12:02:01 +0100

    Render todos (+ smol workshop).

---
+++ 02-mocking/src/tests/data/todos.ts
@@ -0,0 +1,12 @@
+/**
+ * Test-todos
+ */
+import { Todo } from "../../types/Todo";
+
+export const todos: Todo[] = [
+	{ id: 1, title: "My first todo", completed: false },
+	{ id: 2, title: "My second todo", completed: true },
+	{ id: 3, title: "My third todo", completed: false },
+];
+
+export default todos;
--- 02-mocking/src/tests/render.test.ts
+++ 02-mocking/src/tests/render.test.ts
@@ -1,5 +1,11 @@
+import { Window } from "happy-dom";
 import { afterEach, describe, expect, it } from "vitest";
 import { transformTodosToHtml } from "../utils/render";
+import dummyTodos from "./data/todos";
+
+// Create a new instance of Window
+const window = new Window();
+const document = window.document;
 
 describe("render todos", () => {
 
@@ -8,10 +14,19 @@ describe("render todos", () => {
 		expect(html).toBe("");
 	});
 
-	it.todo("outputs a list with one todo");
+	it("outputs a list with one todo", () => {
+		const todoLIs = transformTodosToHtml([ dummyTodos[0] ]);
+
+		document.body.innerHTML = `<ul>${todoLIs}</ul>`;
+
+		// query for any listitems with the class todo
+		expect(document.querySelectorAll("li.todo")).toHaveLength(1);
+		// expect(document.querySelectorAll("li.todo").length).toBe(1); // also works
+	});
 
 	it.todo("outputs a list with one completed todo");
 
-	it.todo("outputs a list with two todos");
+	it.todo("outputs a list with many todos");
 
+	it.todo("outputs a list with one not completed todo");
 });
