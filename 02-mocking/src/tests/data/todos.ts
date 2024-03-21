166cefe08676b11d3dc0b74be701a95126712590
Johan Nordström <johan@digitalvillage.se>
2024-03-21 14:36:11 +0100

    Workshop solution.

--- 02-mocking/src/tests/data/todos.ts
+++ 02-mocking/src/tests/data/todos.ts
@@ -7,6 +7,8 @@ export const todos: Todo[] = [
 	{ id: 1, title: "My first todo", completed: false },
 	{ id: 2, title: "My second todo", completed: true },
 	{ id: 3, title: "My third todo", completed: false },
+	{ id: 4, title: "My fourth todo", completed: true },
+	{ id: 5, title: "My fifth todo", completed: true },
 ];
 
 export default todos;
--- 02-mocking/src/tests/render.test.ts
+++ 02-mocking/src/tests/render.test.ts
@@ -4,10 +4,28 @@ import { transformTodosToHtml } from "../utils/render";
 import dummyTodos from "./data/todos";
 
 // Create a new instance of Window
-const window = new Window();
-const document = window.document;
+// const window = new Window();
+// const document = window.document;
+const { document } = new Window();
+
+// find the first finished todo
+const finishedTodo = dummyTodos.find(todo => todo.completed) ?? {
+	...dummyTodos[0],   // id: 1, title: "", completed: false
+	completed: true,
+};
+
+// find the first unfinished todo
+const unfinishedTodo = dummyTodos.find(todo => !todo.completed) ?? {
+	...dummyTodos[0],   // id: 1, title: "", completed: true
+	completed: false,
+};
+
 
 describe("render todos", () => {
+	// 🧹
+	afterEach(() => {
+		document.body.innerHTML = "";
+	});
 
 	it("outputs empty list when no todos exist", () => {
 		const html = transformTodosToHtml([]);
@@ -24,9 +42,26 @@ describe("render todos", () => {
 		// expect(document.querySelectorAll("li.todo").length).toBe(1); // also works
 	});
 
-	it.todo("outputs a list with one completed todo");
+	it("outputs a list with one completed todo", () => {
+		const todoLIs = transformTodosToHtml([ finishedTodo ]);
+		document.body.innerHTML = `<ul>${todoLIs}</ul>`;
 
-	it.todo("outputs a list with many todos");
+		expect(document.querySelectorAll("li.todo.completed")).toHaveLength(1);
+	});
 
-	it.todo("outputs a list with one not completed todo");
+	it("outputs a list with many todos", () => {
+		const todoLIs = transformTodosToHtml(dummyTodos);
+		document.body.innerHTML = `<ul>${todoLIs}</ul>`;
+
+		expect(document.querySelectorAll("li.todo")).toHaveLength(dummyTodos.length);
+	});
+
+	it("outputs a list with one not completed todo", () => {
+		const todoLIs = transformTodosToHtml([ unfinishedTodo ]);
+		document.body.innerHTML = `<ul>${todoLIs}</ul>`;
+
+		// expect(document.querySelectorAll("li.todo")).toHaveLength(1);
+		// expect(document.querySelectorAll("li.todo.completed")).toHaveLength(0);
+		expect(document.querySelectorAll("li:not(.completed).todo")).toHaveLength(1);
+	});
 });
