5208df88821c1e67465d5119108893840000ccd4
Johan Nordström <johan@digitalvillage.se>
2024-03-21 11:04:34 +0100

    Test that it outputs empty list when no todos exist.

--- 02-mocking/src/tests/render.test.ts
+++ 02-mocking/src/tests/render.test.ts
@@ -1,8 +1,12 @@
 import { afterEach, describe, expect, it } from "vitest";
+import { transformTodosToHtml } from "../utils/render";
 
 describe("render todos", () => {
 
-	it.todo("outputs empty list when no todos exist");
+	it("outputs empty list when no todos exist", () => {
+		const html = transformTodosToHtml([]);
+		expect(html).toBe("");
+	});
 
 	it.todo("outputs a list with one todo");
 
