1637ff2ba9ea31e948d16bb5c00b70e6e9dcb731
Johan Nordström <johan@digitalvillage.se>
2024-03-21 10:58:13 +0100

    Render todos test skeleton.

---
+++ 02-mocking/src/tests/render.test.ts
@@ -0,0 +1,13 @@
+import { afterEach, describe, expect, it } from "vitest";
+
+describe("render todos", () => {
+
+	it.todo("outputs empty list when no todos exist");
+
+	it.todo("outputs a list with one todo");
+
+	it.todo("outputs a list with one completed todo");
+
+	it.todo("outputs a list with two todos");
+
+});
