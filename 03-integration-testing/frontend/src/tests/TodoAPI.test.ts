6481cacb82dca709270df3b6e740c26dc2cdc65c
Johan Nordström <johan@digitalvillage.se>
2024-03-25 11:49:54 +0100

    Added test-skeleton 💀.

---
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import * as TodoAPI from "../services/TodoAPI";
+
+describe("TodoAPI", () => {
+
+	it.todo("should return a list");
+
+	it.todo("should create a todo");
+
+	it.todo("should create and then get the todo");
+
+	it.todo("should create and then find the todo among all todos");
+
+	it.todo("should create and then update the todo");
+
+	it.todo("should create and then delete the todo");
+
+});
