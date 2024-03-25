f6d68a60d4e8735f9326b494d025dc723c41a489
Johan Nordström <johan@digitalvillage.se>
2024-03-25 13:28:14 +0100

    Test that a todo can be created.

--- 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -1,5 +1,11 @@
 import { describe, it, expect } from "vitest";
 import * as TodoAPI from "../services/TodoAPI";
+import { TodoData } from "../types/Todo";
+
+const newTodo: TodoData = {
+	title: "Test todo",
+	completed: false,
+}
 
 describe("TodoAPI", () => {
 
@@ -9,7 +15,22 @@ describe("TodoAPI", () => {
 		expect( Array.isArray(todos) ).toBe(true);
 	});
 
-	it.todo("should create a todo");
+	it("should create a todo", async () => {
+		const todo = await TodoAPI.createTodo(newTodo);
+
+		/*
+		// expect(typeof todo.id).toBe("number");
+		expect(todo.id).toBeTypeOf("number");
+		expect(todo.title).toBe(newTodo.title);
+		expect(todo.completed).toBe(newTodo.completed);
+		*/
+
+		expect(todo).toMatchObject({
+			id: expect.any(Number),
+			title: newTodo.title,
+			completed: newTodo.completed,
+		});
+	});
 
 	it.todo("should create and then get the todo");
 
