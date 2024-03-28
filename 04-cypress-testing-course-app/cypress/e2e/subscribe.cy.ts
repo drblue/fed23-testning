46331928287fcd341f82f11eacb291fb0e9cd557
Johan Nordström <johan@digitalvillage.se>
2024-03-28 13:34:56 +0100

    Added sad path for invalid email, already subscribed email and no email.

--- 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -14,20 +14,33 @@ describe("Newsletter Subscribe", () => {
 	});
 
 	it("allows users to subscribe to the email list", () => {
-		cy.getByDataTest("email-input")
-			.should("be.visible")
-			.type(email);
+		cy.getByDataTest("email-input").should("be.visible").type(email);
 
 		cy.getByDataTest("submit-button").click();
 
 		// API-request
 
-		cy.getByDataTest("success-message")
-			.should("exist")
-			.contains(email);
+		cy.getByDataTest("success-message").should("exist").contains(email);
 	});
 
-	it.skip("displays an error message when the email is invalid");
+	it("displays an error message when the email is invalid", () => {
+		cy.getByDataTest("email-input").type(invalidEmail);
+		cy.getByDataTest("submit-button").click();
+		cy.getByDataTest("success-message").should("not.exist");
+	});
 
-	it.skip("should not allow users to subscribe twice");
+	it("should not allow users to subscribe twice", () => {
+		cy.getByDataTest("email-input").type(existingSubscriberEmail);
+		cy.getByDataTest("submit-button").click();
+		cy.getByDataTest("server-error-message")
+			.should("be.visible")
+			.contains(existingSubscriberEmail);
+	});
+
+	it("should not allow subscribing without an email address", () => {
+		cy.getByDataTest("submit-button").click();
+		cy.getByDataTest("error-message")
+			.should("be.visible")
+			.contains("Email is required");
+	});
 });
