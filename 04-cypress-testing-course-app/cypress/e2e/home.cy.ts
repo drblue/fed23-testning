c777367c28948b419648df006d369c9e2933ae10
Johan Nordström <johan@digitalvillage.se>
2024-03-28 13:38:30 +0100

    Group tests under a context.

--- 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -7,22 +7,24 @@ describe("Home Page", () => {
 		cy.get("#__next").should("be.visible");
 	});
 
-	it("the h1 contains the correct text", () => {
-		// Find the h1 and verify content
-		// cy.get('[data-test="hero-heading"]')  // 🫤
-		cy.get("h1")
-			.should("exist")
-			.contains("Testing Next.js Applications with Cypress");
-	});
+	context("Hero section", () => {
+		it("the h1 contains the correct text", () => {
+			// Find the h1 and verify content
+			// cy.get('[data-test="hero-heading"]')  // 🫤
+			cy.get("h1")
+				.should("exist")
+				.contains("Testing Next.js Applications with Cypress");
+		});
 
-	it("the features on the homepage are correct", () => {
-		// Find the first definition term and verify content
-		cy.get("dt").eq(0).contains("4 Courses");
+		it("the features on the homepage are correct", () => {
+			// Find the first definition term and verify content
+			cy.get("dt").eq(0).contains("4 Courses");
 
-		// Find the second definition term and verify content
-		cy.get("dt").eq(1).contains("25+ Lessons");
+			// Find the second definition term and verify content
+			cy.get("dt").eq(1).contains("25+ Lessons");
 
-		// Find the third definition term and verify content
-		cy.get("dt").eq(2).contains("Free and Open Source");
+			// Find the third definition term and verify content
+			cy.get("dt").eq(2).contains("Free and Open Source");
+		});
 	});
 });
