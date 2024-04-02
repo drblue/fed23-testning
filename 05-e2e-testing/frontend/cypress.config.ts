a0db7773f1297f15946f437121e13c723335dc42
Johan Nordström <johan@digitalvillage.se>
2024-04-02 09:44:31 +0200

    Moved example specs to their own folder and configured Cypress to ignore it.

--- 05-e2e-testing/frontend/cypress.config.ts
+++ 05-e2e-testing/frontend/cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
 	e2e: {
+		excludeSpecPattern: [
+			"**/e2e/examples",
+		],
 		baseUrl: "http://localhost:5173/",
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
