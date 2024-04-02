33ae54bd2a59dd05f210994a666abe348d5fbe7b
Johan Nordström <johan@digitalvillage.se>
2024-04-02 11:33:54 +0200

    Test: can create a new todo (and see it in the list and clears input).

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -18,7 +18,25 @@ describe("Todos", () => {
 			cy.get('#error').should("be.visible").contains("Title cannot be empty");
 		});
 
-		it.skip("can create a new todo (and see it in the list and clears input)");
+		it.only(
+			"can create a new todo (and see it in the list and clears input)",
+			{ defaultCommandTimeout: 6000 },  // wait **up to** 6000 ms when looking for an element
+			() => {
+				const todoTitle = "Too many todos, did not read";
+
+				// type a todo title and submit form
+				cy.get("#new-todo-title").type(todoTitle);
+				cy.get("[type=\"submit\"]").click();
+
+				// cy.wait(1500); // **always** waits 1500 ms, even if the request is quicker
+
+				// expect that a todo with the title exists in the list
+				cy.get("#todos").find("li").last().contains(todoTitle);
+
+				// expect input to be empty
+				cy.get("#new-todo-title").should("have.value", "");
+			}
+		);
 
 		it.skip("can type in the create todo form and then reset the form");
 	});
