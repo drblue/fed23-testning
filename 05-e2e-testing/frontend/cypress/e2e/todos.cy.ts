ac956635ae62582e7c4450db617645835e19c4a1
Johan Nordström <johan@digitalvillage.se>
2024-04-02 11:14:12 +0200

    Test: can't create a todo without a title

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -3,16 +3,20 @@ describe("Todos", () => {
 		cy.visit("/");
 	});
 
-	it("should see at least one todo", () => {
+	it.skip("should see at least one todo", () => {
 		cy.get("#todos").find("li").should("have.length.at.least", 1);
 	});
 
 	context("create todo", () => {
-		it.only("create todo form should be empty", () => {
+		it("create todo form should be empty", () => {
 			cy.get("#new-todo-title").should("have.value", "");
 		});
 
-		it.skip("can't create a todo without a title");
+		it("can't create a todo without a title", () => {
+			// cy.get("#new-todo-title").type("{enter}");
+			cy.get("[type=\"submit\"]").click();
+			cy.get('#error').should("be.visible").contains("Title cannot be empty");
+		});
 
 		it.skip("can create a new todo (and see it in the list and clears input)");
 
