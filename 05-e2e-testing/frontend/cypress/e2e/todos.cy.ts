7fb3e7b4e4f7123e2d42a70e4357fc7d5b9c4c07
Johan Nordström <johan@digitalvillage.se>
2024-04-02 10:12:08 +0200

    Test that we should see at least one todo.

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -1,5 +1,9 @@
 describe("Todos", () => {
-	it("should be able to visit the page", () => {
+	beforeEach(() => {
 		cy.visit("/");
+	});
+
+	it("should see at least one todo", () => {
+		cy.get("#todos").find("li").should("have.length.at.least", 1);
 	});
 });
