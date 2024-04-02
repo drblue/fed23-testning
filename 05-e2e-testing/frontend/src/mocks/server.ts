57f8c4676e1369531bff2c6db513f4a1201cc15c
Johan Nordström <johan@digitalvillage.se>
2024-04-02 09:19:34 +0200

    Deleted old mocks and tests.

--- 05-e2e-testing/frontend/src/mocks/server.ts
+++
@@ -1,5 +0,0 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-// Configure a request mocking server with our handlers
-export const server = setupServer(...handlers);
