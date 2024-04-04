50986b7bd5b9e5830786e8f984d1eaa7d2cbe44f
Johan Nordström <johan@digitalvillage.se>
2024-04-04 11:38:45 +0200

    Test: should click on the first todo and land on a page with the todo's id in the URL.

--- 06-e2e-testing-102/cypress/e2e/todos.cy.ts
+++ 06-e2e-testing-102/cypress/e2e/todos.cy.ts
@@ -5,7 +5,7 @@ const snelhest = {
 
 describe("Firebase Todos", () => {
 
-	context("can't access protected routes without logging in", () => {
+	context.skip("can't access protected routes without logging in", () => {
 		it("should be redirected to login when accessing todos page as a guest", () => {
 			cy.visit("/todos");
 			cy.location("pathname").should("eq", "/login");
@@ -13,7 +13,7 @@ describe("Firebase Todos", () => {
 		});
 	});
 
-	context("can log in", () => {
+	context.skip("can log in", () => {
 		beforeEach(() => {
 			cy.login(snelhest.email, snelhest.password);   // so nice 🤩
 		});
@@ -29,6 +29,36 @@ describe("Firebase Todos", () => {
 		it("should log in with an existing user and visit todos page", () => {
 			cy.visit("/todos");
 			cy.location("pathname").should("eq", "/todos");
+		});
+	});
+
+	context("todo actions", () => {
+		beforeEach(() => {
+			cy.login(snelhest.email, snelhest.password);   // so nice 🤩
+		});
+
+		afterEach(() => {
+			cy.logout();
+		});
+
+		it("should click on the first todo and land on a page with the todo's id in the URL", () => {
+			cy.visit("/todos");
+
+			// get the first todo item in the list
+			cy.get(".todo-list-item")
+				.first()
+				.invoke("attr", "data-todo-id")
+				.then((todoId) => {
+					cy.log(`Got me some todoId: ${todoId}`);
+
+					cy.get(".todo-list-item")
+						.first()
+						.click();
+
+					cy.wait(500);
+
+					cy.location("pathname").should("eq", "/todos/" + todoId);
+				});
 		});
 	});
 });
