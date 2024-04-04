974a1efce7e261dd175152155b4975fdf2d1b411
Johan Nordström <johan@digitalvillage.se>
2024-04-04 10:17:59 +0200

    Added baseUrl and created our first tests for logging in.

--- 06-e2e-testing-102/cypress/e2e/todos.cy.ts
+++ 06-e2e-testing-102/cypress/e2e/todos.cy.ts
@@ -1,5 +1,37 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const snelhest = {
+	email: "snelhest2000@gmail.com",
+	password: "appapp",
+}
+
+describe("Firebase Todos", () => {
+
+	context("can't access protected routes without logging in", () => {
+		it("should be redirected to login when accessing todos page as a guest", () => {
+			cy.visit("/todos");
+			cy.location("pathname").should("eq", "/login");
+			// cy.location("pathname").should("not.eq", "/todos");
+		});
+	});
+
+	context("can log in", () => {
+		beforeEach(() => {
+			cy.visit("/login");
+		});
+
+		afterEach(() => {
+			cy.visit("/logout");
+			cy.wait(1500);
+		});
+
+		it("should log in with an existing user", () => {
+			cy.get("input[type=\"email\"]").type(snelhest.email, { delay: 10 });
+			cy.get("input[type=\"password\"]").type(snelhest.password);
+			cy.get("[type=\"submit\"]").click();
+			cy.wait(1000);
+
+			// cy.login(snelhest.email, snelhest.password);   // wouldn't it be nice
+
+			cy.location("pathname").should("eq", "/");
+		});
+	});
+});
